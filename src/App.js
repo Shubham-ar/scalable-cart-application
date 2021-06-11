24c0f55f67f3dcec2dec7473b900bca9ff74e94c
Shubham-ar <shubham.jhamb2000@gmail.com>
2021-06-12 01:14:27 +0530

    commit on master

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'react-toastify/dist/ReactToastify.css'
 import './App.css';
+import { toast } from 'react-toastify';
 
 function App() {
+
+  const [cartItem, setCartItem] = useState([]);
+
+
+  const addCart = item => {
+
+    const isAlreadyAdded = cartItem.findIndex(function (array) {
+      return array.id === item.id
+    })
+
+    if (isAlreadyAdded !== -1) {
+      return toast("already added in cart", {
+        type: "error"
+      })
+    }
+
+    setCartItem([...cartItem, item])
+
+  }
+
   return (
     <div className="App">
-     
+
     </div>
   );
 }
