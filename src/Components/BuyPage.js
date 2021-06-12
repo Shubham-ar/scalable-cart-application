66241af52d05e40817b59a6aa15c15cbf452e2bd
Shubham-ar <shubham.jhamb2000@gmail.com>
2021-06-12 16:34:15 +0530

    commit on master

--- src/Components/BuyPage.js
+++ src/Components/BuyPage.js
@@ -1,1 +1,6 @@
-import React,{useEffect,useState} from 'react'
+import React, { useEffect, useState } from 'react'
+
+import { random, commerce } from 'faker'
+import { Container, Row, Col } from 'reactstrap'
+
+const apikey = "563492ad6f9170000100000119e5de6ec2434fad852d052a59fe50c0"
